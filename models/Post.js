5025559c464cfea29fa6845e53052a68766936a2
Allure <samira-h@hotmail.co.uk>
2022-07-01 22:59:30 +0100

    Dependencies

---
+++ models/Post.js
@@ -0,0 +1,6 @@
+// Post model
+
+// Dependencies
+// sequelize model, datatypes, and database connection
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');

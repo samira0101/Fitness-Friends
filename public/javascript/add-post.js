8c20ac2f42905fab4df5b634b09808f19c00c486
Allure <samira-h@hotmail.co.uk>
2022-07-01 23:03:52 +0100

    add a new post, use the create a new post

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -6,3 +6,14 @@ async function newFormHandler(event) {
     const title = document.querySelector('input[name="post-title"]').value;
     const post_text = document.querySelector('textarea[name="post-text"]').value;
 
+    // To add a new post, use the create a new post POST route. The post user id is taken from the route's session information.
+    const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        post_text
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });

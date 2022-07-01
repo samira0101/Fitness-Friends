a85511211b4ad0d8cea04a30ed11445ee8a2b2e4
Humayra Hayern <hayern786@hotmail.com>
2022-07-01 22:35:30 +0100

    posting new comment function

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -21,3 +21,20 @@ router.get("/", (req, res) => {
     });
 });
 
+// Posting a new comment
+router.post("/", withAuth, (req, res) => {
+  // check the session, and if it exists, create a comment
+  if (req.session) {
+    Comment.create({
+      comment_text: req.body.comment_text,
+      post_id: req.body.post_id,
+      // use the user id from the session
+      user_id: req.session.user_id,
+    })
+      .then((dbCommentData) => res.json(dbCommentData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
+});

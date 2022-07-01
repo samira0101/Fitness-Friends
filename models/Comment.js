36905e5e8fbc112efcb2c5c27a4ce79d9bffc157
Allure <samira-h@hotmail.co.uk>
2022-07-01 22:54:58 +0100

    comment model dependencies

---
+++ models/Comment.js
@@ -0,0 +1,6 @@
+// Comment model
+
+// Dependencies
+// sequelize model, datatypes, and database connection
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');

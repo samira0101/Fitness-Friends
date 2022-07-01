706e83bd3c0ca37ffd797633efcd2c4c6443676e
Allure <samira-h@hotmail.co.uk>
2022-07-01 23:09:08 +0100

    Getting post title and post text from the form

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -6,3 +6,7 @@ async function editFormHandler(event) {
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
+
+    // Getting post title and post text from the form
+    const title = document.querySelector('input[name="post-title"]').value;
+    const post_text = document.querySelector('textarea[name="post-text"]').value;

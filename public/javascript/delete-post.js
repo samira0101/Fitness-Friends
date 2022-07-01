69053f37f95d03b8da25f8624201c19a48d3b355
Allure <samira-h@hotmail.co.uk>
2022-07-01 23:07:23 +0100

    if delete is successful redirect to dashboard

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -10,3 +10,14 @@ async function deleteFormHandler(event) {
     const response = await fetch(`/api/posts/${id}`, {
         method: 'DELETE'
       });
+    // if the delete action is successful, redirect to the dashboard page, otherwise display the error
+    if (response.ok) {
+        document.location.replace('/dashboard');
+        // otherwise, display the error
+      } else {
+        alert(response.statusText);
+      }
+  }
+  
+
+  document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);

20305b9567012cd9d19fdc315c6121fd7b1e0241
Humayra Hayern <hayern786@hotmail.com>
2022-07-01 22:40:12 +0100

    get users

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -10,3 +10,20 @@ const withAuth = require("../../utils/auth");
 // Sequelize store to save the session so the user can remain logged in
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
+// Routes
+
+// GET /api/users -- get all users
+router.get("/", (req, res) => {
+  // Access the User model and run .findAll() method to get all users
+  User.findAll({
+    // When sending the data back, omit off the password property.
+    attributes: { exclude: ["password"] },
+  })
+    // return the data as JSON formatted
+    .then((dbUserData) => res.json(dbUserData))
+    // if server error, return that error
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});

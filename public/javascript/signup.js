28f64f75ee4ed658b7be29ba73e78f5b92f95c1c
Allure <samira-h@hotmail.co.uk>
2022-07-01 23:14:39 +0100

    Sign up form handler

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -7,3 +7,26 @@ async function signupFormHandler(event) {
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
+    
+
+    if (username && email && password) {
+         // POST the new user to the user table in the database
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            headers: {'Content-Type': 'application/json'}
+        });
+        if (response.ok) {
+            alert('Account created! Logging you in now.');
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText)
+        }
+    }
+}
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);

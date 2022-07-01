b283d108a2bf3577ce9ffee044965c0fd6131a34
Allure <samira-h@hotmail.co.uk>
2022-07-01 23:03:19 +0100

    Get the post title and post text from the form

---
+++ public/javascript/add-post.js
@@ -0,0 +1,8 @@
+// New Post Form Handler
+async function newFormHandler(event) {
+    event.preventDefault();
+
+    // Get the post title and post text from the form
+    const title = document.querySelector('input[name="post-title"]').value;
+    const post_text = document.querySelector('textarea[name="post-text"]').value;
+

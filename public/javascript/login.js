57d46b4a20d34cee0068c740cc7baf88c688ed2d
Allure <samira-h@hotmail.co.uk>
2022-07-01 23:12:46 +0100

    Login form handler

---
+++ public/javascript/login.js
@@ -0,0 +1,8 @@
+// Login form handler
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    // Collect values from the login form
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+

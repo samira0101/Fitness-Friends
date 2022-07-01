08c1c456a992c9dd48ef67936d240865a2177c63
Allure <samira-h@hotmail.co.uk>
2022-07-01 22:56:14 +0100

    index file to gather the models and export

---
+++ models/index.js
@@ -0,0 +1,8 @@
+// An index file to gather the models and export them for use
+
+// User model
+const User = require('./User');
+// Post model
+const Post = require('./Post');
+// Comment model
+const Comment = require('./Comment');

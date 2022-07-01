7f6f67a77e062fc172007b569997bf19d6908568
Humayra Hayern <hayern786@hotmail.com>
2022-07-01 22:39:41 +0100

    delete post function

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -117,3 +117,25 @@ router.put("/:id", withAuth, (req, res) => {
       res.status(500).json(err);
     });
 });
+
+// DELETE api/posts/1 -- delete a post
+router.delete("/:id", withAuth, (req, res) => {
+  Post.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
